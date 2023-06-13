0bd78e0d8c25b9e2558437049f1dc140e667f6fb
Kantita01 <kantita5069@gmail.com>
2023-06-13 13:06:23 +0700

    first commit

---
+++ bin/dart_week5.dart
@@ -0,0 +1,11 @@
+void main(){
+  print("Assignment Operators");
+  print("==========");
+  const int n1 = 10;
+  const int n2 = 5;
+  print("n1+n2 = ${n1 + n2}");
+  print("n1-n2 = ${n1 - n2}");
+  print("n1*n2 = ${n1 * n2}");
+  print("n1/n2 = ${n1 / n2}");
+  print("n1%n2 = ${n1 % n2}");
+}
